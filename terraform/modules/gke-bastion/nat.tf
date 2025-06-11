a6b553b1ee99eab187b9c8558301b199e05a7077
shiv935055 <155819700+shiv935055@users.noreply.github.com>
2025-06-11 12:23:43 +0530

    Create nat.tf

---
+++ terraform/modules/gke-bastion/nat.tf
@@ -0,0 +1,14 @@
+# nat.tf
+resource "google_compute_router" "nat_router" {
+  name    = var.nat_router_name
+  network = google_compute_network.vpc_network.name
+  region  = var.region
+}
+resource "google_compute_router_nat" "nat" {
+  name                               = var.nat_name
+  router                             = google_compute_router.nat_router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+  nat_ips                            = [google_compute_address.global_ip.self_link]
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
