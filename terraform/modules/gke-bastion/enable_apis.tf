2e122a5b1c8d1746f2098e42aa948cf9d2c25cf1
shiv935055 <155819700+shiv935055@users.noreply.github.com>
2025-06-11 12:08:52 +0530

    Create enable_apis.tf

---
+++ terraform/modules/gke-bastion/enable_apis.tf
@@ -0,0 +1,19 @@
+# enable_apis.tf
+locals {
+  enabled_apis = [
+    "serviceusage.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "compute.googleapis.com",
+    "servicenetworking.googleapis.com",
+    "container.googleapis.com",
+    "gkehub.googleapis.com"
+  ]
+}
+resource "google_project_service" "enabled_apis" {
+  for_each = toset(local.enabled_apis)
+  service  = each.value
+  project  = var.project_id
+}
+resource "null_resource" "prep" {
+  depends_on = [google_project_service.enabled_apis]
+}
