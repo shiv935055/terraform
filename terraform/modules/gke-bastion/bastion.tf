4410478004504e831b683e72ac929bc1861e470a
shiv935055 <155819700+shiv935055@users.noreply.github.com>
2025-06-11 15:13:46 +0530

    Update bastion.tf

--- terraform/modules/gke-bastion/bastion.tf
+++ terraform/modules/gke-bastion/bastion.tf
@@ -1,29 +1,38 @@
-# bastion.tf
 resource "google_compute_instance" "bastion" {
   name         = var.bastion_name
   machine_type = var.bastion_machine_type
   zone         = "${var.region}-a"
+
   boot_disk {
     initialize_params {
       image = var.bastion_image
     }
   }
+
   network_interface {
     network    = google_compute_network.vpc_network.self_link
     subnetwork = google_compute_subnetwork.subnetwork.self_link
     access_config {}
   }
+
   metadata_startup_script = var.bastion_startup_script
-  tags = var.bastion_tags
+  tags                    = var.bastion_tags
+
+  service_account {
+    email  = google_service_account.bastion_sa.email
+    scopes = ["cloud-platform"] # You can replace this with limited scopes if preferred
+  }
 }
 
 resource "google_compute_firewall" "allow_ssh_bastion" {
   name    = var.firewall_name
   network = google_compute_network.vpc_network.self_link
+
   allow {
     protocol = "tcp"
     ports    = var.firewall_ports
   }
+
   source_ranges = var.firewall_source_ranges
   target_tags   = var.firewall_target_tags
 }
@@ -32,6 +41,7 @@ resource "google_service_account" "bastion_sa" {
   account_id   = "bastion-sa"
   display_name = "Bastion Service Account"
 }
+
 resource "google_project_iam_member" "bastion_sa_roles" {
   for_each = toset(var.service_account_roles)
   project  = var.project_id
